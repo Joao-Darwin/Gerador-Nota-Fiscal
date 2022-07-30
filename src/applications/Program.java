db380c1cf3199996ddb3d9332c4625baa9229856
Darwin <joaodarwin.ip22@gmail.com>
2022-07-30 17:52:16 -0300

    Criação do repositorio

---
+++ src/applications/Program.java
@@ -0,0 +1,9 @@
+package applications;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/entities/NotaFiscal.java
@@ -0,0 +1,57 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class NotaFiscal {
+	
+	private Integer numeroNota;
+	private Date data;
+	
+	private List<Produto> produtos = new ArrayList<E>();
+	private Pagamento pagamento;
+	
+	public NotaFiscal () {
+	}
+	
+	public NotaFiscal (Integer numeroNota, Date data, Pagamento pagamento) {
+		this.numeroNota = numeroNota;
+		this.data = data;
+		this.pagamento = pagamento;
+	}
+
+	public Integer getNumeroNota() {
+		return numeroNota;
+	}
+
+	public void setNumeroNota(Integer numeroNota) {
+		this.numeroNota = numeroNota;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	public Pagamento getPagamento() {
+		return pagamento;
+	}
+
+	public void setPagamento(Pagamento pagamento) {
+		this.pagamento = pagamento;
+	}
+	
+	
+}
